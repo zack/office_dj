2569e85be31eef0b2d02ff8f717cf3c7315e211c
Zack Youngren <zack@youngren.io>
2017-10-24 22:42:59 -0400

    ..
    Listing 7.20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      # handle save
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
